f05da56376c2e3ae583060c1cbbe343db92635c7
edrew08 <eric.r.drew@gmail.com>
2016-03-15 10:25:19 -0500

    add tab bar

---
+++ movie-rec/TabController.swift
@@ -0,0 +1,18 @@
+//
+//  TabController.swift
+//  movie-rec
+//
+//  Created by Eric Drew on 3/15/16.
+//  Copyright © 2016 Eric Drew. All rights reserved.
+//
+
+import UIKit
+
+class TabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+    }
+
+}
