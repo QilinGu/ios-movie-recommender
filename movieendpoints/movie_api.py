b98ced5107842a9264abd77b4d4c4311820ace3f
edrew08 <eric.r.drew@gmail.com>
2016-03-24 12:26:13 -0500

    deploy to cloud

--- movieendpoints/movie_api.py
+++ movieendpoints/movie_api.py
@@ -2,6 +2,9 @@
 """/Applications/Google\ Chrome.app/Contents//MacOS/Google\ Chrome --user-data-dir=test --unsafely-treat-insecure-origin-as-secure=http://localhost:8080"""
 """http://localhost:8080/_ah/api/explorer"""
 """dev_appserver.py  helloendpoints"""
+"""appcfg.py update helloendpoints"""
+"""https://stately-forest-126023.appspot.com/_ah/api/movierec/v1/similarMovie/5"""
+"""https://stately-forest-126023.appspot.com/_ah/api/movierec/v1/movieRec/5"""
 
 import endpoints
 from protorpc import messages
@@ -16,16 +19,25 @@ package = 'Movie'
 class MovieApi(remote.Service):
 
   ID_RESOURCE = endpoints.ResourceContainer(message_types.VoidMessage,
-      id=messages.IntegerField(1, variant=messages.Variant.INT32))
+      id=messages.IntegerField(1, variant=messages.Variant.INT32, required=True))
 
-  @endpoints.method(ID_RESOURCE, simModel.MovieId,
+  @endpoints.method(ID_RESOURCE, simModel.MovieList,
                     path='similarMovie/{id}', http_method='GET',
                     name='movie.getSimilar')
   def similar_get(self, request):
     try:
       return simModel.getSimilar(request.id)
-    except (IndexError, TypeError):
-      raise endpoints.NotFoundException('Greeting %s not found.' %(request.id,))
+    except:
+      raise endpoints.NotFoundException('%s not found.' %(request.id,))
+
+  @endpoints.method(ID_RESOURCE, simModel.MovieRec,
+                    path='movieRec/{id}', http_method='GET',
+                    name='movie.getRec')
+  def rec_get(self, request):
+    try:
+      return simModel.getRec(request.id)
+    except:
+      raise endpoints.NotFoundException('%s not found.' %(request.id,))
 
 APPLICATION = endpoints.api_server([MovieApi])
 
--- movieendpoints/similar_model.py
+++ movieendpoints/similar_model.py
@@ -2,10 +2,17 @@
 import endpoints
 from protorpc import messages
 
-class MovieId(messages.Message):
+class MovieList(messages.Message):
   items = messages.StringField(1, repeated=True)
+
+class MovieRec(messages.Message):
+  movieId = messages.StringField(1)
 
 #movieId is an int
 def getSimilar(movieId):
 	movieIdList = ['9999', '8888', '7777', str(movieId)]
-	return MovieId(items=movieIdList)
+	return MovieList(items=movieIdList)
+
+#movieId is an int
+def getRec(movieId):
+	return MovieRec(movieId="12345")
