b2aa14c03d2213ecec287cfabb531c6176c03529
edrew08 <eric.r.drew@gmail.com>
2016-03-25 14:40:47 -0500

    update endpoint

--- movieendpoints/similar_model.py
+++ movieendpoints/similar_model.py
@@ -1,6 +1,8 @@
 
 import endpoints
+from google.appengine.ext import ndb
 from protorpc import messages
+import protorpc
 
 class MovieList(messages.Message):
   items = messages.StringField(1, repeated=True)
@@ -8,10 +10,26 @@ class MovieList(messages.Message):
 class MovieRec(messages.Message):
   movieId = messages.StringField(1)
 
+#def lookup(client, key):
+#    key = client.key('similarMovieId', key)
+#    task = client.get(key)
+#    result = []
+#    for i in range(1, 11):
+#        result.append(task[str(i)])
+#    return result
+
+#movieId is an int
+#def getSimilar(movieId):
+#	result = lookup(client, str(movieId))
+#	return MovieList(items=result)
+
+
 #movieId is an int
 def getSimilar(movieId):
-	movieIdList = ['9999', '8888', '7777', str(movieId)]
-	return MovieList(items=movieIdList)
+  movieIdList = ['9999', '8888', '7777', str(movieId)]
+  return MovieList(items=movieIdList)
+
+
 
 #movieId is an int
 def getRec(movieId):
